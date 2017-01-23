eb791122b296c333d2d40b95e08852d6e86c6c68
asantibanez <santibanez.andres@gmail.com>
2017-01-23 15:32:29 -0500

    Added Has Many relationships

---
+++ test/has-many-test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+const Promise = require('bluebird');
+const Expect = require('expect.js');
+const User = require('./models/user');
+const Customer = require('./models/customer');
+
+const dynamoDocClient = require('./utils/dynamo-doc-client');
+
+
+describe('Has One tests', function() {
+
+    it('Instance should return relate model', () => {
+        return User.first(dynamoDocClient)
+            .then(user => {
+                return Promise.all([
+                    user,
+                    user.customer()
+                ]);
+            }).spread((user, customer) => {
+                Expect(user).to.not.be(null);
+                Expect(customer).to.not.be(null);
+                Expect(customer).to.be.a(Customer);
+                Expect(customer.email).to.be(user.email);
+            });
+    });
+
+});
